4a1bb13943d2fe46f003950fb38f0b25d5facdc6
shimotachi3 <mariajie.bg0@gmail.com>
2025-04-15 12:22:01 +0200

    dom finished

--- dom/app.js
+++ dom/app.js
@@ -1,1 +1,9 @@
-// ¿En tu páis de origen es normal dejar propina?
+
+document.getElementById('calculate').addEventListener('click', function () {
+    const bill = document.getElementById('bill-amount').value;
+    const tip = document.getElementById('percentage-tip').value;
+    const tipAmount = (bill*tip/100);
+    
+    document.getElementById('tip-amount').value = tipAmount.toFixed(2);
+    document.getElementById('total').value = (tipAmount + parseFloat(bill)).toFixed(2);
+});
